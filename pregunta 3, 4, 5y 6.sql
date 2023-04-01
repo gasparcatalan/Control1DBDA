2aed1eaee2f5d2090ba67601e9f41f434a311c75
Elena Guzmán <91577901+strxngers@users.noreply.github.com>
2023-03-31 23:24:34 -0300

    [Ivania Elena Guzmán] Preguntas 3 y 6 resueltas

--- pregunta 3, 4, 5y 6.sql
+++ pregunta 3, 4, 5y 6.sql
@@ -1,3 +1,11 @@
+-- Pregunta 3
+-- Ventas por mes, separadas entre Boletas y Facturas
+select tc.tipo, extract(month from v.fecha), count(*) as ventas_mes 
+from venta v, tipo_doc tc
+where v.id_tipo_doc = tc.id_doc
+group by v.fecha, tc.tipo
+order by v.fecha asc;
+
 --Pregunta 4
 --Empleado que ganó más por tienda 2020, indicando la comuna donde vive y el cargo
 --que tiene en la empresa
@@ -11,7 +19,17 @@ and e.id_empleado = s.id_empleado
 and s.fecha >= '2020-01-01 '
 and s.fecha < '2021-01-01'
 order by s.total desc
-limit 1
+limit 1;
+
+-- Pregunta 5: 
+-- La tienda que tiene menos empleados
+
+select t.nombre, count(*) as cant_empleados
+from tienda t, tienda_emp te
+where t.id_tienda = te.id_tienda
+group by t.nombre
+order by cant_empleados asc
+limit 1;
 
 --Pregunta 6
 --El vendedor con más ventas por mes
@@ -27,5 +45,6 @@ where v.id_vendedor = pv.id_vendedor
 and pv.id_venta = ve.id_venta
 group by v.id_vendedor, extract(month from ve.fecha), extract(year from ve.fecha)
 order by count(distinct ve.id_venta) desc
+
 
 -- resultado cercano al esperado, pero aun incorrecto
